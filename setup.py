6886ac7e5c66d428d5f92c4a65a89062878d267a
eliphatfs <flandre@scarletx.cn>
2023-11-21 14:48:59 -0800

    Initialize structure.

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("caliborn/version.py", "r") as fh:
+    exec(fh.read())
+    __version__: str
+
+
+def packages():
+    return setuptools.find_packages(exclude=['tests'])
+
+
+setuptools.setup(
+    name="caliborn",
+    version=__version__,
+    author="flandre.info",
+    author_email="flandre@scarletx.cn",
+    description="Toolbox for converting rotations, camera calibration matrices, transforms and spaces.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eliphatfs/caliborn",
+    packages=packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy', 'typing_extensions'
+    ],
+    python_requires='~=3.6',
+)
