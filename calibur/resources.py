2efdd32cfcde57360cceed44e243c1698ecd024f
eliphatfs <flandre@scarletx.cn>
2023-11-24 16:26:03 -0800

    Add more test objects.

--- calibur/resources.py
+++ calibur/resources.py
@@ -4,3 +4,31 @@ from .generic_utils import get_relative_path
 
 def get_blender_cube() -> trimesh.Trimesh:
     return trimesh.load(get_relative_path("blender_cube.obj"))
+
+
+def get_spot() -> trimesh.Trimesh:
+    """
+    The spot mesh from Keenan's 3D Model Repository
+    "Robust fairing via conformal curvature flow"
+    The mesh is in Unity coordinates (Z forward, Y up).
+    The animal looks into -Z.
+    """
+    return trimesh.load(get_relative_path("spot_quadrangulated.obj"))
+
+
+def get_monkey() -> trimesh.Trimesh:
+    """
+    The blender monkey head mesh.
+    The mesh is in GL coordinates (-Z forward, Y up).
+    The animal looks into Z.
+    """
+    return trimesh.load(get_relative_path("monkey.obj"))
+
+
+def get_monkey_glb() -> trimesh.Trimesh:
+    """
+    The blender monkey head mesh in GLB format.
+    The mesh is in GLTF coordinates (-Z forward, Y up).
+    The animal looks into Z.
+    """
+    return trimesh.load(get_relative_path("monkey.obj"))
